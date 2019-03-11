82b1c83f14b27b0fbb43bd55f4cc71b3e8879e92
Carlos Wirama (aleph mac) <carloswirama@gmail.com>
2019-03-11 20:42:04 +0800

    refactor api services

--- src/api.js
+++ src/api.js
@@ -1,34 +1,54 @@
 import wtf from 'wtf_wikipedia';
 
 const DOMAIN = `https://m.bulbapedia.bulbagarden.net/w/api.php`;
-const DEFAULT_PARAMS = `?origin=*&action=parse&format=json&redirects=1`;
+// const DEFAULT_PARAMS = `?origin=*&action=parse&format=json&redirects=1`;
+const DEFAULT_PARAMS = {
+  origin: '*',
+  action: 'parse',
+  format: 'json',
+  redirects: 1,
+};
 const POKEMON_LIST_PAGE = `List_of_Pokémon_by_Kanto_Pokédex_number`;
 
-export function buildQueryParams(params) { // TODO: obj above is just an example
+export function buildQueryParams(params) {
+  function buildKeyValueQueryFromObject(obj) {
+    return Object.keys(obj).map(key => `${key}=${obj[key]}`)
+  }
   return [
-    DEFAULT_PARAMS,
-    ...Object.keys(params).map(key => `${key}=${params[key]}`),
+    ...buildKeyValueQueryFromObject(DEFAULT_PARAMS),
+    ...buildKeyValueQueryFromObject(params),
   ].join('&');
 }
 
 async function fetchWiki(params) {
   const queryParams = buildQueryParams(params);
-  const url = `${DOMAIN}${queryParams}`;
+  const url = `${DOMAIN}?${queryParams}`;
   const response =  await fetch(url).then(r => r.json())
   return response.parse;
 }
 
-async function fetchAndParseWiki(params) {
+async function fetchAndParseWiki(params, sections) {
   const { wikitext } = await fetchWiki({ ...params, prop: 'wikitext' });
-  // return await wtf(wikitext['*']).json();
-  return await wtf(wikitext['*']).sections('By leveling up').json();
+  // TODO; should break this down to allow multple sections in one fetch
+  return await wtf(wikitext['*']).sections(sections).json();
+}
+
+export async function getPokemons() {
+  // get all Kanto Pokemon (Pokemon Let's Go)
+  const learnsetSection = await fetchAndParseWiki({
+    page: POKEMON_LIST_PAGE,
+  }, 'By leveling up');
+  const learnsetTable = learnsetSection.templates
+    .filter(i => i.template === 'learnlist/level7');
+
+  return await learnsetTable;
 }
 
 export async function getPokemonDetail(name) {
   // get learnset table
   const learnsetSection = await fetchAndParseWiki({
     page: name,
-  });
+  }, 'By leveling up');
   const learnsetTable = learnsetSection.templates
     .filter(i => i.template === 'learnlist/level7');
 
