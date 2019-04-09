c3af125b99ffd1c7f96350c5c4b2729424b2d7bb
Carlos Wirama (aleph mac) <carloswirama@gmail.com>
2019-04-10 00:03:03 +0800

    styling typography and margin

--- src/pages/Search/SearchResultItem.styled.js
+++ src/pages/Search/SearchResultItem.styled.js
@@ -8,7 +8,7 @@ export const StyledListItem = styled(ListItem)`
   && {
     background-color: rgba(32, 129, 255, 0.3);
     border-radius: 10px 28px;
-    margin: 24px 0;
+    margin: 16px 0;
     padding: 4px 8px;
     box-shadow: 0 0 8px 10px rgba(182, 237, 255, 0.3);
     border: solid 3px rgba(176, 235, 255, 0.8);
@@ -40,13 +40,22 @@ export const Name = styled.div`
 export const Types = styled.div`
   display: flex;
   color: white;
+  padding-top: 8px;
 `;
 
 export const Type = styled.div`
+  font-family: "Roboto Mono";
   display: flex;
   color: white;
   text-transform: uppercase;
   background-color: ${props => getTypeColor(props.children)};
-  font-size: 12pt;
-  // line-spacing: .5px;
+  font-size: 12px;
+  padding: 4px 8px 6px;
+  letter-spacing: -0.3px;
+  border-radius: 5px;
+  box-shadow: inset 0px -2px rgba(0,0,0,0.2), inset 0px 1px rgba(255,255,255,0.2);
+
+  &:last-child {
+    margin-left: 4px;
+  }
 `;
