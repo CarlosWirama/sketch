7ad50d640577b3602a5e310a7c3fea4354a3b641
Carlos Wirama (aleph mac) <carloswirama@gmail.com>
2019-05-12 23:53:06 +0800

    Updates

---
+++ precache-manifest.d9f72284ec52b79ca04feaaab28afad7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a3649815e40709ca063e69bcf09d5851",
+    "url": "/pokeplanner/static/media/RobotoMono-Regular.a3649815.ttf"
+  },
+  {
+    "revision": "369144aa521d4034b5a8",
+    "url": "/pokeplanner/static/js/runtime~main.1b3e0fdf.js"
+  },
+  {
+    "revision": "629bfd4a0c479578c357",
+    "url": "/pokeplanner/static/js/main.742d9acd.chunk.js"
+  },
+  {
+    "revision": "01b63e7e8006a7eab224",
+    "url": "/pokeplanner/static/js/2.fc509120.chunk.js"
+  },
+  {
+    "revision": "629bfd4a0c479578c357",
+    "url": "/pokeplanner/static/css/main.f73bb69e.chunk.css"
+  },
+  {
+    "revision": "2b126117ccce8d2c688c83b0e607eefe",
+    "url": "/pokeplanner/index.html"
+  }
+];
