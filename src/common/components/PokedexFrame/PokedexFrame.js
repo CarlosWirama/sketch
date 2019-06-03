905abc9fd316d5e5aa8e63a800483ad5b0da8d44
Carlos Wirama (aleph mac) <carloswirama@gmail.com>
2019-06-04 01:28:07 +0800

    added pokedex buttons

--- src/common/components/PokedexFrame/PokedexFrame.js
+++ src/common/components/PokedexFrame/PokedexFrame.js
@@ -2,6 +2,11 @@ import React from 'react';
 import {
   Container,
   LeftFrame,
+  TopButtonsContainer,
+  BottomButtonsContainer,
+  WhiteButton,
+  RectangleButtonsContainer,
+  PlusAndCircleButtonsContainer,
   LeftFrameLines,
   BigCircleBorder,
   PokeballLogo,
@@ -18,10 +23,27 @@ export default function PokedexFrame() {
   return (
     <Container>
       <LeftFrame>
+        <TopButtonsContainer>
+          <WhiteButton shape="circle" size="24px" left="40px" />
+          <WhiteButton shape="circle" size="72px" left="55px" />
+          <WhiteButton shape="circle" size="24px" left="70px" />
+          <WhiteButton width="9px" height="48px" left="145px" top="-30px"/>
+        </TopButtonsContainer>
         <LeftFrameLines/>
         <BigCircleBorder>
           <PokeballLogo/>
         </BigCircleBorder>
+        <BottomButtonsContainer>
+          <RectangleButtonsContainer>
+            <WhiteButton width="45px" height="24px" left="40px" />
+            <WhiteButton width="45px" height="24px" left="50px" />
+            <WhiteButton width="45px" height="24px" left="60px" />
+          </RectangleButtonsContainer>
+          <PlusAndCircleButtonsContainer>
+            <WhiteButton shape="plus" size="36px" left="40px" />
+            <WhiteButton shape="circle" size="30px" left="135px" />
+          </PlusAndCircleButtonsContainer>
+        </BottomButtonsContainer>
         <Recess />
       </LeftFrame>
       <RightFrame>
