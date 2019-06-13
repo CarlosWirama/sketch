5d1c9ee0df08ce772af9a4b831c8ff3ecaf11355
Carlos Wirama (aleph mac) <carloswirama@gmail.com>
2019-06-13 22:22:29 +0800

    added Physical move icon

--- src/common/components/MoveCategoryIcon.js
+++ src/common/components/MoveCategoryIcon.js
@@ -3,17 +3,68 @@ import styled from 'styled-components';
 
 export default function MoveCategoryIcon({ category, ...props }) {
   switch(category) {
+    case 'Physical': return <Physical {...props}/>;
     case 'Special': return <Special {...props}/>;
     case 'Status': return <Status {...props}/>;
     default: return null;
   }
 }
 
+function Physical(props) {
+  return (
+    <PhysicalContainer  {...props}>
+      <PhysicalStarVerticalHorizontal/>
+      <PhysicalStarDiagonal />
+    </PhysicalContainer>
+  );
+}
+
+const shadow = '0 1px 2px rgba(0, 0, 0, 0.5)';
+const filterShadow = '0 1px 0.5px rgba(0, 0, 0, 0.5)';
+const boxShadow = `inset ${shadow}, ${shadow}`;
+
 const Template = styled.span`
-  box-shadow:
-    inset 0 1px 2px 0 rgba(0, 0, 0, 0.5),
-    0 1px 2px 0 rgba(0, 0, 0, 0.5);
   margin-right: 4px;
+`;
+
+const PhysicalContainer = styled(Template)`
+  position: relative;
+  padding-right: 16px;
+  filter: drop-shadow(${filterShadow});
+`;
+
+const PhysicalStarTemplate = styled.div`
+  position: absolute;
+  &:before, &:after {
+    content: "";
+    position: absolute;
+    background-color: #ffcd00;
+  }
+  &:before {
+    transform: skew(22.5deg, 22.5deg);
+  }
+  &:after {
+    transform: skew(-22.5deg, -22.5deg);
+  }
+`;
+
+const PhysicalStarDiagonal = styled(PhysicalStarTemplate)`
+  top: 4px;
+  left: 4px;
+  &:before, &:after {
+    width: 8px;
+    height: 8px;
+  }
+`;
+
+const PhysicalStarVerticalHorizontal = styled(PhysicalStarTemplate)`
+  transform: rotate(45deg);
+  top: 2px;
+  left: 8px;
+  &:before, &:after {
+    width: 7.5px;
+    height: 7.5px;
+  }
 `;
 
 const Special = styled(Template)`
@@ -27,13 +78,11 @@ const Special = styled(Template)`
     background-color: #2266cc;
     border: 1px solid #b9eeff;
     border-radius: 50%;
+    box-shadow: ${boxShadow};
   }
   &:before, &:after {
     content: "";
     position: absolute;
-    box-shadow:
-      0 1px 2px 0 rgba(0, 0, 0, 0.5);
-      inset 0 1px 2px 0 rgba(0, 0, 0, 0.5),
   }
   &:before {
     width: 70%;
@@ -55,6 +104,7 @@ const Status = styled(Template)`
   border-width: 1px 1px 8.5px 1px;
   border-radius: 50%;
   position: relative;
+  box-shadow: ${boxShadow};
   &:before, &:after {
     content: "";
     position: absolute;
