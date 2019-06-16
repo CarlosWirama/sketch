f4ff3d169ac0000b049c0e727f3fe0bf218e533f
Carlos Wirama (aleph mac) <carloswirama@gmail.com>
2019-06-16 11:13:15 +0800

    Updates

--- precache-manifest.8d6b033917ce77d38e0237a0a569f673.js
+++ precache-manifest.8d6b033917ce77d38e0237a0a569f673.js
@@ -8,19 +8,19 @@ self.__precacheManifest = [
     "url": "/pokeplanner/static/js/runtime~main.1b3e0fdf.js"
   },
   {
-    "revision": "eace99d7f3a222d3b110",
-    "url": "/pokeplanner/static/js/main.5a3a4e98.chunk.js"
+    "revision": "425a3d197d7a4b0d14b3",
+    "url": "/pokeplanner/static/js/main.5e7e438c.chunk.js"
   },
   {
-    "revision": "d21e767cab7e7098463d",
-    "url": "/pokeplanner/static/js/2.8638ee8e.chunk.js"
+    "revision": "194e1ceb1e553d644bd6",
+    "url": "/pokeplanner/static/js/2.3d31ea20.chunk.js"
   },
   {
-    "revision": "eace99d7f3a222d3b110",
+    "revision": "425a3d197d7a4b0d14b3",
     "url": "/pokeplanner/static/css/main.f73bb69e.chunk.css"
   },
   {
-    "revision": "180c90fab2307227a08f44c44bd9158c",
+    "revision": "f3c799f0e9e90c6768d21ed4cbdcb006",
     "url": "/pokeplanner/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/pokeplanner/precache-manifest.b6277611d751e02ce7841b0255b73482.js"
+  "/pokeplanner/precache-manifest.8d6b033917ce77d38e0237a0a569f673.js"
 );
 
 workbox.clientsClaim();
