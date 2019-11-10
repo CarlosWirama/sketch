469d2d6b9960634ff2d02dd838524c29af9dc78a
CarlosWirama <carloswirama@gmail.com>
2019-11-10 12:50:26 +0800

    added marking icons

--- src/common/components/Marking/Marking.tsx
+++ src/common/components/Marking/Marking.tsx
@@ -1,10 +1,26 @@
 import React, { useState } from 'react';
 import IconButton from '@material-ui/core/IconButton';
-import StarIcon from '@material-ui/icons/Star';
-import StarBorderIcon from '@material-ui/icons/StarBorder';
 import { MarkingConstant } from './enums';
 import { Color } from 'csstype';
-  
+
+import Pokeball from '@material-ui/icons/Android';
+import PokeballOutline from '@material-ui/icons/Android';
+import Bigstar from '@material-ui/icons/Android';
+import BigstarOutline from '@material-ui/icons/Android';
+import Circle from '@material-ui/icons/Lens';
+import CircleOutline from '@material-ui/icons/RadioButtonUnchecked';
+import Triangle from '@material-ui/icons/Android';
+import TriangleOutline from '@material-ui/icons/ChangeHistory';
+import Star from '@material-ui/icons/Star';
+import StarOutline from '@material-ui/icons/StarBorder';
+import Heart from '@material-ui/icons/Favorite';
+import HeartOutline from '@material-ui/icons/FavoriteBorder';
+import Square from '@material-ui/icons/Android';
+import SquareOutline from '@material-ui/icons/CheckBoxOutlineBlank';
+import Diamond from '@material-ui/icons/Bookmark';
+import DiamondOutline from '@material-ui/icons/BookmarkBorder';
+import { SvgIconProps } from '@material-ui/core/SvgIcon';
+
 interface Marking {
   pokeball: MarkingConstant;
   bigstar: MarkingConstant;
@@ -30,31 +46,20 @@ const defaultMarking: Marking = {
 export default function Marking() {
   const [marks, setMarks] = useState(defaultMarking);
 
-  const markingIcons = { // TODO later change this strings into icons
-    pokeball: '',
-    bigstar: '',
-    circle: '',
-    triangle: '',
-    star: '',
-    heart: '',
-    square: '',
-    diamond: '',
-  };
   function getMarkColor(markValue: MarkingConstant): Color {
-    switch (MarkingConstant[markValue]) {
-      // case MarkingConstant.BLUE_MARK: return 'blue';
-      case 'BLUE_MARK': return 'blue';
-      case 'RED_MARK': return 'red';
-      default: return 'lightgray';
+    switch (markValue) {
+      case MarkingConstant.BLUE_MARK: return 'blue';
+      case MarkingConstant.RED_MARK: return 'red';
+      default: return 'darkgray-3';
     }
   }
 
-  function onMark(markName: keyof Marking): void {
-    const currMarkValue = marks[markName];
+  function onMark(markKey: keyof Marking): void {
+    const currMarkValue = marks[markKey];
     const newMarkValue: MarkingConstant = (currMarkValue + 1) % 3;
     setMarks({
       ...marks,
-      [markName]: newMarkValue,
+      [markKey]: newMarkValue,
     });
   }
 
@@ -63,15 +68,45 @@ export default function Marking() {
   >;
 
   return (
-    <div style={{ display: 'flex', flexDirection: 'row' }}>
+    <div style={{ display: 'flex', flexWrap: 'wrap', flexDirection: 'row' }}>
         {markEntries.map(([markKey, markValue]) => (
-        <IconButton onClick={() => onMark(markKey)} key={markKey}>
-            {(markValue === MarkingConstant.UNMARKED)
-            ? <StarBorderIcon />
-            : <StarIcon style={{ color: getMarkColor(markValue) }}/>
-            }
-        </IconButton>
+          <MarkIcon
+            key={markKey}
+            markKey={markKey}
+            markValue={markValue}
+            onMark={onMark}
+            style={{ color: getMarkColor(markValue) }}
+          />
         ))}
     </div>
   );
+}
+
+
+function MarkIcon({ markKey, markValue, onMark, ...props }: MarkIconProps) {
+  const useBordered = markValue === MarkingConstant.UNMARKED;
+  let Icon: React.ComponentType<SvgIconProps>;
+  switch (markKey) {
+    case 'pokeball': Icon = useBordered ? PokeballOutline : Pokeball; break;
+    case 'bigstar': Icon = useBordered ? BigstarOutline : Bigstar; break;
+    case 'circle': Icon = useBordered ? CircleOutline : Circle; break;
+    case 'triangle': Icon = useBordered ? TriangleOutline : Triangle; break;
+    case 'star': Icon = useBordered ? StarOutline : Star; break;
+    case 'heart': Icon = useBordered ? HeartOutline : Heart; break;
+    case 'square': Icon = useBordered ? SquareOutline : Square; break;
+    case 'diamond': Icon = useBordered ? DiamondOutline : Diamond; break;
+    default: Icon = useBordered ? PokeballOutline : Pokeball;
+  }
+  return (
+    <IconButton onClick={() => onMark(markKey)} key={markKey}>
+      <Icon {...props} />
+    </IconButton>
+  );
+}
+
+interface MarkIconProps {
+  markKey: keyof Marking;
+  markValue: MarkingConstant;
+  onMark: (markKey: keyof Marking) => void;
+  style?: any; // can't find any suitable type
 }
