0de23a4095c89ed04796d72c77808d93d112c94c
CarlosWirama <carloswirama@gmail.com>
2019-12-14 13:17:39 +0800

    Updates

---
+++ precache-manifest.1fb2456620e9aec39a2541964b87335e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a3649815e40709ca063e69bcf09d5851",
+    "url": "/pokeplanner/static/media/RobotoMono-Regular.a3649815.ttf"
+  },
+  {
+    "revision": "369144aa521d4034b5a8",
+    "url": "/pokeplanner/static/js/runtime~main.1b3e0fdf.js"
+  },
+  {
+    "revision": "4eab10e23fb93e95a8cc",
+    "url": "/pokeplanner/static/js/main.a86661b9.chunk.js"
+  },
+  {
+    "revision": "fce702dc421ee2d85ca6",
+    "url": "/pokeplanner/static/js/2.04bf4e83.chunk.js"
+  },
+  {
+    "revision": "4eab10e23fb93e95a8cc",
+    "url": "/pokeplanner/static/css/main.f613843d.chunk.css"
+  },
+  {
+    "revision": "92a7e5824051dcb8632d6119fb8e9584",
+    "url": "/pokeplanner/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3137412090bfe6bbb5f8e38b064617b7.js"
+  "/pokeplanner/precache-manifest.1fb2456620e9aec39a2541964b87335e.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/pokeplanner/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
