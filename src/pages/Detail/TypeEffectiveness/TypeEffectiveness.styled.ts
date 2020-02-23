379a3e80037025663ab582fcb19c4bd4f18fbdca
CarlosWirama <carloswirama@gmail.com>
2020-02-23 14:15:47 +0800

    fixed minor css - min-width issue on animation - removed center alignment

--- src/pages/Detail/TypeEffectiveness/TypeEffectiveness.styled.ts
+++ src/pages/Detail/TypeEffectiveness/TypeEffectiveness.styled.ts
@@ -3,7 +3,7 @@ import { BorderedCard } from '../../../common/components/Card';
 
 export const EffectivenessCategory = styled.div<{ active: boolean }>`
   transition: flex 0.3s cubic-bezier(0.15, 0.3, 0.25, 1) 0s;
-  flex: ${props => props.active ? 3 : 1};
+  flex: ${props => props.active ? 2.25 : 1};
   display: flex;
   flex-direction: column;
   min-width: 80px;
@@ -12,18 +12,17 @@ export const EffectivenessCategory = styled.div<{ active: boolean }>`
 export const TypeListContainer = styled(BorderedCard)`
   flex: 1;
   display: flex;
-  justify-content: space-evenly;
+  justify-content: space-between;
   flex-wrap: wrap;
-  align-items: center;
   margin: 8px 4px 0;
-  padding: 8px 0;
+  padding: 8px;
 `;
 
 export const AnimatedWidth = styled.div`
   transition: width .3s cubic-bezier(0.15, 0.3, 0.25, 1);
-  min-width: 28px;
   width: 88px;
+  overflow: hidden;
   &.short {
-    width: 0;
+    width: 28px;
   }
 `;
