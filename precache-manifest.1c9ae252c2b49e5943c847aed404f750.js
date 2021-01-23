ee1850eb10bfa16f5feb04d8fb3fb0968de5f295
CarlosWirama <carloswirama@gmail.com>
2021-01-23 18:39:35 +0800

    Updates

---
+++ precache-manifest.1c9ae252c2b49e5943c847aed404f750.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "dc8a9a56121e1466f30c",
+    "url": "/sketch/static/css/main.1664bcc8.chunk.css"
+  },
+  {
+    "revision": "dc8a9a56121e1466f30c",
+    "url": "/sketch/static/js/main.88c84d40.chunk.js"
+  },
+  {
+    "revision": "240b49737d5ee7462b49",
+    "url": "/sketch/static/js/runtime~main.d7fbe05f.js"
+  },
+  {
+    "revision": "e985f027d45f98c0aaec",
+    "url": "/sketch/static/js/2.357b2c53.chunk.js"
+  },
+  {
+    "revision": "a3649815e40709ca063e69bcf09d5851",
+    "url": "/sketch/static/media/RobotoMono-Regular.a3649815.ttf"
+  },
+  {
+    "revision": "0b5f0a28e07fafa624f587d674f97ca1",
+    "url": "/sketch/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/pokeplanner/precache-manifest.39090e90ba3abee76b1eea08ca2a39d9.js"
+  "/sketch/precache-manifest.1c9ae252c2b49e5943c847aed404f750.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/pokeplanner/index.html", {
+workbox.routing.registerNavigationRoute("/sketch/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
