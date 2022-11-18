909b986a7e09570aae60259187eb5f8cb5a36585
Carlos <carloswirama@gmail.com>
2022-11-18 13:30:04 +0700

    Updates

--- precache-manifest.d499afa755af0fe21aa4f82700dd9e90.js
+++ precache-manifest.d499afa755af0fe21aa4f82700dd9e90.js
@@ -1,26 +1,26 @@
 self.__precacheManifest = [
   {
-    "revision": "1e93640da629f3f77200",
+    "revision": "5798b5d4f0667281e594",
     "url": "/sketch/static/css/main.cf2cabc8.chunk.css"
   },
   {
-    "revision": "1e93640da629f3f77200",
-    "url": "/sketch/static/js/main.08ff4275.chunk.js"
+    "revision": "5798b5d4f0667281e594",
+    "url": "/sketch/static/js/main.4e513063.chunk.js"
   },
   {
     "revision": "240b49737d5ee7462b49",
     "url": "/sketch/static/js/runtime~main.d7fbe05f.js"
   },
   {
-    "revision": "57a22c321dd11fd5fde7",
-    "url": "/sketch/static/js/2.278027c2.chunk.js"
+    "revision": "e494285ff88c80dd1ac0",
+    "url": "/sketch/static/js/2.c9776de2.chunk.js"
   },
   {
     "revision": "a3649815e40709ca063e69bcf09d5851",
     "url": "/sketch/static/media/RobotoMono-Regular.a3649815.ttf"
   },
   {
-    "revision": "343b5cc4bb7f8215d3112a0440501bea",
+    "revision": "834df89e5e8d97bb528b96213dfb6529",
     "url": "/sketch/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/sketch/precache-manifest.d66efd867ab589da38ee9dc6d910b1eb.js"
+  "/sketch/precache-manifest.d499afa755af0fe21aa4f82700dd9e90.js"
 );
 
 workbox.clientsClaim();
